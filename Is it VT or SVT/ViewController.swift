502d0ff95a3e9fb2d8a46e20d5643ac9599bec43
Abraham Salacata <asalacata@abraham-salacatas-macbook-pro.local>
2017-03-24 20:52:50 -0400

    Initial Commit

---
+++ Is it VT or SVT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Is it VT or SVT
+//
+//  Created by Abraham Salacata on 3/24/17.
+//  Copyright © 2017 Abraham Salacata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Is it VT or SVTTests/Is_it_VT_or_SVTTests.swift
@@ -0,0 +1,36 @@
+//
+//  Is_it_VT_or_SVTTests.swift
+//  Is it VT or SVTTests
+//
+//  Created by Abraham Salacata on 3/24/17.
+//  Copyright © 2017 Abraham Salacata. All rights reserved.
+//
+
+import XCTest
+@testable import Is_it_VT_or_SVT
+
+class Is_it_VT_or_SVTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
